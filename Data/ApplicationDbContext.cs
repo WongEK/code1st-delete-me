00323330222146eb0d5377016cc44c83af7b2c59
Edmond Wong <wongedmondk@gmail.com>
2022-01-28 09:41:37 -0800

    Initial Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Code1st.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Code1st.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Team>? Teams { get; set; }
+    public DbSet<Player>? Players { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Team>().HasData(SampleData.GetTeams());
+        modelBuilder.Entity<Player>().HasData(SampleData.GetPlayers());
+    }
+   
+}
---
+++ Data/Migrations/20220125005440_Manager.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Code1st.Data.Migrations
+{
+    public partial class Manager : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Manager",
+                table: "Teams",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Manager",
+                table: "Teams");
+        }
+    }
+}
